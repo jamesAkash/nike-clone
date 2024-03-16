f5bd08907708372284987d9212eddbfff61e8daf
jamesAkash <akash.james@outlook.com>
2024-03-16 11:08:18 +0530

    fixed advertisement

--- src/sections/Advertisement.jsx
+++ src/sections/Advertisement.jsx
@@ -8,11 +8,11 @@ const Advertisement = () => {
       <p className="font-bold">Shop all our new markdowns</p>
 
       <img loading="lazy" className=" my-4" src={nikeAd} alt="Advertisement" />
-      <p className="font-bold md:text-xl sm:-mb-3">Coming Soon</p>
-      <h1 className="text-[40px] text-center break-words text-xl md:text-[60px] font-extrabold">
+      <p className="font-bold md:text-xl -mb-1 sm:mb-3 ">Coming Soon</p>
+      <h1 className="text-[40px] text-center  text-xl md:text-[60px] font-extrabold">
         NIKE AIR MAX DN
       </h1>
-      <p className="font-bold text-center md:text-xl sm:-mt-2">
+      <p className="font-bold text-center md:text-xl -mt-1 sm:mt-3">
         The next generation of Air technology launches April 24/24
       </p>
     </div>
